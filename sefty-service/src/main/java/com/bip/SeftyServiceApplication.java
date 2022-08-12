cf9090d5f7b95e6fde8132ffafff57ea647137a8
Rahul Anand <rahul.anand435@gmail.com>
2022-08-12 12:20:36 +0530

    Test by Rahul
    Test by Rahul

--- sefty-service/src/main/java/com/bip/SeftyServiceApplication.java
+++ sefty-service/src/main/java/com/bip/SeftyServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeftyServiceApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SeftyServiceApplication.class, args);
 	}
